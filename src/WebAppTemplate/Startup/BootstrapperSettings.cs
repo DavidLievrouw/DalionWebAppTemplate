9e40f515ed7bb95334f0581744be7e48ffa5582e
David Lievrouw <david.lievrouw@recomatics.com>
2019-05-04 20:06:47 +0200

    Make the IIS Express launch profile work.

--- src/WebAppTemplate/Startup/BootstrapperSettings.cs
+++ src/WebAppTemplate/Startup/BootstrapperSettings.cs
@@ -4,6 +4,7 @@ namespace Dalion.WebAppTemplate.Startup {
     internal class BootstrapperSettings {
         public string EnvironmentName { get; set; }
         public bool UseDetailedErrors { get; set; }
+        public bool DisableHttpsRedirection { get; set; }
         public Assembly EntryAssembly { get; set; }
     }
 }
